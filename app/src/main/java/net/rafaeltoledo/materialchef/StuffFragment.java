862ee67f2f1bb77bd8a20467802b2964f1b072fd
Rafael Toledo <rafaeldtoledo@gmail.com>
2015-09-18 18:51:46 -0300

    TextInputLayout

--- app/src/main/java/net/rafaeltoledo/materialchef/StuffFragment.java
+++ app/src/main/java/net/rafaeltoledo/materialchef/StuffFragment.java
@@ -1,8 +1,17 @@
 package net.rafaeltoledo.materialchef;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class StuffFragment extends Fragment {
 
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stuff, container, false);
+    }
 }
