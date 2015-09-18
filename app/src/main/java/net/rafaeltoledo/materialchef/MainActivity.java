b86ef46e4aaa33f2d4aaf06e94831cb06af9f9a8
Rafael Toledo <rafaeldtoledo@gmail.com>
2015-09-18 18:07:24 -0300

    SnackBar

--- app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
@@ -1,15 +1,19 @@
 package net.rafaeltoledo.materialchef;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private RecyclerView mRecyclerView;
+    private FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(new MainAdapter());
+
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, R.string.hello_world, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
