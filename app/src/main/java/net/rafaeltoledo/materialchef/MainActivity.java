0c37bdd37194bbcdb6bead0e99755e45bc47ec42
Rafael Toledo <rafaeldtoledo@gmail.com>
2016-10-19 20:09:23 -0200

    Adds example with BottomNavigationView

--- app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
@@ -1,51 +1,42 @@
 package net.rafaeltoledo.materialchef;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-import static android.support.v4.app.ActivityOptionsCompat.*;
+import static android.support.v4.app.ActivityOptionsCompat.makeSceneTransitionAnimation;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener,
         View.OnClickListener {
-
-    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
-        if (navigationView != null) {
-            navigationView.setNavigationItemSelectedListener(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
         }
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        if (navigationView != null) {
+            navigationView.setOnNavigationItemSelectedListener(this);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
-            toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    drawerLayout.openDrawer(GravityCompat.START);
-                }
-            });
         }
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
@@ -54,37 +45,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
             tabLayout.setupWithViewPager(viewPager);
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Snackbar.make(v, R.string.app_name, Snackbar.LENGTH_LONG)
-                            .setAction(R.string.action, new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-
-                                }
-                            })
-                            .show();
-                }
-            });
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        drawerLayout.closeDrawer(GravityCompat.START);
+        // Navigation goes here :)
         return true;
     }
 
