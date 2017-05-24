ee5b93bcf071805541ff2d6a5c06284cc42f49ab
Rafael Toledo <rafaeldtoledo@gmail.com>
2017-05-24 16:42:12 -0300

    Update

--- app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/materialchef/MainActivity.java
@@ -2,6 +2,7 @@ package net.rafaeltoledo.materialchef;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -83,7 +84,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
